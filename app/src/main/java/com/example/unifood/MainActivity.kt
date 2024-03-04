a53945676c7682ee2c2ffad0d90c25d68c615074
Daniel Garcia <da.garciap1@uniandes.edu.co>
2024-03-03 21:40:34 -0500

    'Borrador_vista_inicial_y_vista_signup'

--- app/src/main/java/com/example/unifood/MainActivity.kt
+++ app/src/main/java/com/example/unifood/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.example.unifood
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        titleTextViewIntro.text = "Unifood"
-        subtitleTextViewIntro.text = "It's all here"
-        loginButtonIntro.text = "Login"
-        signUpButtonIntro.text = "Sign Up"
+        val signUpButton: Button = findViewById(R.id.signUpButton)
+        val loginButton: Button = findViewById(R.id.loginButton)
+
+        signUpButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        loginButton.setOnClickListener {
+            // Aquí puedes poner el código para ir a la pantalla de inicio de sesión
+        }
     }
 }
+
+
+
