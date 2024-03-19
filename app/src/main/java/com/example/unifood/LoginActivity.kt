9b60e77c2e3c7a7913c2ae07f0bd40d429bd9122
Daniel Garcia <da.garciap1@uniandes.edu.co>
2024-03-19 10:50:35 -0500

    'login_firebase'

--- app/src/main/java/com/example/unifood/LoginActivity.kt
+++ app/src/main/java/com/example/unifood/LoginActivity.kt
@@ -7,15 +7,23 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        auth = FirebaseAuth.getInstance()
+
         val signUpLinkTextView: TextView = findViewById(R.id.signUpLinkTextView)
         val loginButton: Button = findViewById(R.id.loginButton)
         val emailEditText: EditText = findViewById(R.id.emailEditText)
+        val passwordEditText: EditText = findViewById(R.id.passwordEditText)
 
         signUpLinkTextView.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
@@ -24,13 +32,40 @@ class LoginActivity : AppCompatActivity() {
 
         loginButton.setOnClickListener {
             val email = emailEditText.text.toString().trim()
+            val password = passwordEditText.text.toString()
 
             if (email.endsWith("@uniandes.edu.co")) {
-                Toast.makeText(this, "User authenticated successfully!", Toast.LENGTH_SHORT).show()
+                signIn(email, password)
             } else {
                 Toast.makeText(this, "Please enter a valid Uniandes email address", Toast.LENGTH_SHORT).show()
             }
         }
     }
-}
 
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+
+            Toast.makeText(this, "Login successful!", Toast.LENGTH_SHORT).show()
+
+        } else {
+            // Handle unsuccessful login
+            Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
